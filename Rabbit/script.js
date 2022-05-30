7a53f587f4dd72e6f9c23fbfd9c21b1a62f63d7c
Mariam0906 <martirosyan.mariam1999@gmail.ru>
2022-05-30 23:00:35 +0100

    commit

--- Rabbit/script.js
+++ Rabbit/script.js
@@ -1,36 +1,49 @@
 
+// theBeginningOfAdventures() {
+//     createMatrix()
+//     characterPosition()
+//     progressOfEvents()
+// }
+// characterPosition(){
+//     rabbitPosition()
+//     wolvesPosition()
+//     homePosition()
+//     fencePosition()
+// }
+const rabbit = 1
+const wolf = 2
+const fence = 3
+const home = 4
 
-///function createArray(){
-    let arr = new Array()
-    for (let i = 0; i < 5; i++) {
-        arr[i] = new Array()
-        for (let j = 0; j < 5; j++){
-            arr[i][j] = Math.floor(Math.random() * 25)
+const MATRIX_SIZE = document.getElementById("select").value
+console.log(MATRIX_SIZE)
+function createMatrix(MATRIX_SIZE){
+    let array = new Array
+    for (i = 0; i < MATRIX_SIZE; i++){
+        array[i] = new Array()
+        for(j = 0; j < MATRIX_SIZE; j++){
+            array[i][j] = null
         }
-    } 
- //  return arr
-///}
-console.log(arr)
-
-let testArray = [
-[" "," ", " ", " ", " "],
-[6,7,8,9,10],
-[11,12,13,14,15],
-[16,17,18,19,20],
-[21,22,23,24,25]
-]
-// console.log(testArray)
-
-function getRandomElement(array){
-    let random = array[Math.floor(Math.random())];
-    return random
+    }
+    return array
 }
-console.log(getRandomElement(arr))
+console.log(createMatrix(MATRIX_SIZE))
+
+function findRandomFreeCoord(array){
+    let rand1 = Math.floor(Math.random() * array.length);
+    // let rand2 = Math.floor(Math.random() * array.length);
+    console.log(array[rand1][0])
+}
+function rabbitPosition(){
+    const newArray = (createMatrix(MATRIX_SIZE)).map((elem)=>findRandomFreeCoord(createMatrix(MATRIX_SIZE)))
+    return newArray
+}
+rabbitPosition()
 
 
+findRandomFreeCoord(createMatrix(MATRIX_SIZE))
 
 
-///for(let i  = 0, i < )
 
 
 
--- testCaods.js
+++ testCaods.js
@@ -36,3 +36,37 @@
 //   shuffle(arr);
 //   console.log(arr);
 
+
+
+///function createArray(){
+    let arr = new Array()
+    for (let i = 0; i < 5; i++) {
+        arr[i] = new Array()
+        for (let j = 0; j < 5; j++){
+            arr[i][j] = Math.floor(Math.random() * 25)
+        }
+    } 
+ //  return arr
+///}
+console.log(arr)
+
+let testArray = [
+[" "," ", " ", " ", " "],
+[6,7,8,9,10],
+[11,12,13,14,15],
+[16,17,18,19,20],
+[21,22,23,24,25]
+]
+// console.log(testArray)
+
+function getRandomElement(array){
+    let random = array[Math.floor(Math.random())];
+    return random
+}
+console.log(getRandomElement(arr))
+
+
+
+
+///for(let i  = 0, i < )
+ 
