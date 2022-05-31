be3e7a44412c0f2b51dffb61fed8241208c90521
Mariam0906 <martirosyan.mariam1999@gmail.ru>
2022-05-31 10:48:17 +0100

    commit

--- Rabbit/script.js
+++ Rabbit/script.js
@@ -4,49 +4,78 @@
 //     characterPosition()
 //     progressOfEvents()
 // }
-// characterPosition(){
-//     rabbitPosition()
-//     wolvesPosition()
-//     homePosition()
-//     fencePosition()
-// }
+
+   
+
+const elemValue = null
 const rabbit = 1
 const wolf = 2
 const fence = 3
 const home = 4
-
-const MATRIX_SIZE = document.getElementById("select").value
-console.log(MATRIX_SIZE)
-function createMatrix(MATRIX_SIZE){
-    let array = new Array
-    for (i = 0; i < MATRIX_SIZE; i++){
-        array[i] = new Array()
-        for(j = 0; j < MATRIX_SIZE; j++){
-            array[i][j] = null
-        }
+ function createMatrix(size){
+        let matrix = new Array
+            for (i = 0; i < size; i++){
+                matrix[i] = new Array()
+                for(j = 0; j < size; j++){
+                    matrix[i][j] = elemValue
+                }
+            }
+        return matrix
     }
-    return array
+const button = document.getElementById("startButton")
+button.onclick = function (){
+    const MATRIX_SIZE = document.getElementById("select").value
+    let array = createMatrix(MATRIX_SIZE)
+    charactersPosition(array)
+    console.log(array)
 }
-let array = createMatrix(MATRIX_SIZE)
-console.log(array)
 
 function findRandomFreeCoord(array){
-    let rand1 = Math.floor(Math.random() * array.length);
-    
-    // let rand2 = Math.floor(Math.random() * array.length);
-    return array[rand1][0]
+    const x = Math.floor(Math.random() * array.length);
+    const y = Math.floor(Math.random() * array.length);
+    if( array[x][y] === null ){
+        return [x,y]
+    }else{
+       return findRandomFreeCoord(array)
+    }
 }
-
-
+function createPositionForCharacters(array, character){
+    const [x,y] = findRandomFreeCoord(array)
+    array[x][y] = character
+    
+}
 function rabbitPosition(array){
-    let randomCoord = findRandomFreeCoord(createMatrix(MATRIX_SIZE))
-    
-    // const newArray = (createMatrix(MATRIX_SIZE)).map((elem)=>findRandomFreeCoord(createMatrix(MATRIX_SIZE)))
-    // return newArray
-return array.splice(randomCoord, "1")
-
+    createPositionForCharacters(array, "Rabbit")
 }
-console.log(rabbitPosition(array))
+function wolvesPosition(array, wolfCount){
+    for(i = 0; i < wolfCount; i++){
+        createPositionForCharacters(array, "Wolf")
+    } 
+}
+function fencePosition(array,fenceCount){
+    for(i = 0; i < fenceCount; i++){
+        createPositionForCharacters(array, "Fence")
+    }
+}
+function homePosition(array){
+    createPositionForCharacters(array, "Home")
+}
+function charactersPosition(array){
+    rabbitPosition(array)
+    wolvesPosition(array, 3)
+    fencePosition(array, 2)
+    homePosition(array) 
+}
+
+    
+// wolfCount = 3 ///5x5
+// wolfCount = 5 ///7x7
+// wolfCount = 6 ///10x10
+// fenceCount = 2 ///5x5
+// fenceCount = 3///7x7
+// fenceCount = 4 ///10x10
+
+
 
 
 
